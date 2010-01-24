f7b822ff6e8cc7784fa0abc97b53505571119dfc
Rich Cavanaugh <rich@withoutscope.com>
2010-01-23 19:24:27 -0500

    build out the CI notifier a bit

--- Services/Hoptoad/CodeIgniter.php
+++ Services/Hoptoad/CodeIgniter.php
@@ -1,3 +1,22 @@
 <?php
 class Services_Hoptoad_CodeIgniter extends Services_Hoptoad {
+	var $ci;
+
+	function ci() {
+		if (isset($this->ci)) return $this->ci;
+		$this->ci =& get_instance();
+		return $this->ci;
+	}
+
+	function action() {
+		return $this->ci()->router->method;
+	}
+
+	function component() {
+		return $this->ci()->router->class;
+	}
+
+	function project_path() {
+		return APPPATH;
+	}
 }
