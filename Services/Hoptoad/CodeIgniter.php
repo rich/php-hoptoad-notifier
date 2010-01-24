77182248dbb846a4acab1f744f207d5996b296d1
Rich Cavanaugh <rich@withoutscope.com>
2010-01-23 22:09:42 -0500

    simplify the CI class

--- Services/Hoptoad/CodeIgniter.php
+++ Services/Hoptoad/CodeIgniter.php
@@ -2,18 +2,16 @@
 class Services_Hoptoad_CodeIgniter extends Services_Hoptoad {
 	var $ci;
 
-	function ci() {
-		if (isset($this->ci)) return $this->ci;
+	function __construct() {
 		$this->ci =& get_instance();
-		return $this->ci;
 	}
 
 	function action() {
-		return $this->ci()->router->method;
+		return $this->ci->router->method;
 	}
 
 	function component() {
-		return $this->ci()->router->class;
+		return $this->ci->router->class;
 	}
 
 	function project_path() {
